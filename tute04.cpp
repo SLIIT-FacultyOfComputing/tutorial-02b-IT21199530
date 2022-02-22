4beb1cc4210fc00145a3899b6ad78be0c18d2b02
IT21199530 <99311828+IT21199530@users.noreply.github.com>
2022-02-22 19:49:19 +0000

    completed

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,36 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  int fac = 1;
+    for (int r=no; r >= 1; r--) {
+        fac = fac * r;
+    }
+    return fac;
+}
+long nCr(int n, int r)
+
+{
+  int fac1=1;
+  int fac2=1;
+  int fac3=1;
+  int ncr;
+  int x,y,z;
+  for (int x=n; x >= 1; x--) {
+        fac1 = fac1 * x;
+    }
+ 
+  for (int y=r; y >= 1; y--) {
+        fac2 = fac2 * y;
+    }
+  
+   for (int z=(n-r); z >= 1; z--) {
+        fac3 = fac3 * z;
+    }
+
+  ncr=fac1/(fac2*fac3);
+
+return ncr;
+  
+}
